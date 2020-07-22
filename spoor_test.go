cdc65d45cc04f2b5715f26754a1de055c0ed89be
Abdul Gani <abdulg@gmail.com>
2020-07-21 21:30:48 -0400

    Change to table test.

--- spoor_test.go
+++ spoor_test.go
@@ -8,20 +8,33 @@ import (
 )
 
 func TestInfo(t *testing.T) {
-	var infoBuf bytes.Buffer
-	var debugBuf bytes.Buffer
-	info := log.New(&infoBuf, "info: ", 0)
-	debug := log.New(&debugBuf, "debug: ", 0)
-	testSpoor := spoor.New(info, debug)
-	infoExpected := "info: Hello, world\n"
-	debugExpected := ""
-
-	testSpoor.Infof("Hello, %s", "world")
-	if infoBuf.String() != infoExpected {
-		t.Errorf("Expected '%s', received '%s'", infoExpected, infoBuf.String())
+	var tests = []struct {
+		infoLog       string
+		debugLog      string
+		infoExpected  string
+		debugExpected string
+	}{
+		{"Hello, info", "", "info: Hello, info\n", ""},
 	}
+	for _, test := range tests {
+		var infoBuf bytes.Buffer
+		var debugBuf bytes.Buffer
+		info := log.New(&infoBuf, "info: ", 0)
+		debug := log.New(&debugBuf, "debug: ", 0)
+		testSpoor := spoor.New(info, debug)
 
-	if debugBuf.String() != "" {
-		t.Errorf("Expected '%s', received '%s'", debugExpected, infoBuf.String())
+		if test.infoLog != "" {
+			testSpoor.Infof("%s", test.infoLog)
+			if infoBuf.String() != test.infoExpected {
+				t.Errorf("Expected '%s', received '%s'", test.infoExpected, infoBuf.String())
+			}
+		}
+
+		if test.debugLog != "" {
+			testSpoor.Debugf("%s", test.debugLog)
+			if infoBuf.String() != test.debugExpected {
+				t.Errorf("Expected '%s', received '%s'", test.debugExpected, debugBuf.String())
+			}
+		}
 	}
 }
